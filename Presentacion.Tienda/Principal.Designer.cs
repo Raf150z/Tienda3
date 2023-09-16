334bf62e751f96a116a16e55161e8ed33593d6ec
Rafael Velazquez <rafavel555@gmail.com>
2023-09-16 14:27:37 -0600

    Se agregaron tres carpetas (Tienda, PresentacionTienda y vs)

---
+++ Presentacion.Tienda/Principal.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Presentacion.Tienda
+{
+    partial class Frm_Tienda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_descripcion = new System.Windows.Forms.TextBox();
+            this.txt_precio = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_buscar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dtg_Tienda = new System.Windows.Forms.DataGridView();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Tienda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(553, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Precio";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(87, 33);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(205, 20);
+            this.txt_nombre.TabIndex = 4;
+            // 
+            // txt_descripcion
+            // 
+            this.txt_descripcion.Location = new System.Drawing.Point(87, 65);
+            this.txt_descripcion.Name = "txt_descripcion";
+            this.txt_descripcion.Size = new System.Drawing.Size(416, 20);
+            this.txt_descripcion.TabIndex = 5;
+            // 
+            // txt_precio
+            // 
+            this.txt_precio.Location = new System.Drawing.Point(87, 102);
+            this.txt_precio.Name = "txt_precio";
+            this.txt_precio.Size = new System.Drawing.Size(205, 20);
+            this.txt_precio.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_nombre);
+            this.groupBox1.Controls.Add(this.txt_precio);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_descripcion);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(528, 154);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_buscar);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 172);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(528, 81);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscar";
+            // 
+            // txt_buscar
+            // 
+            this.txt_buscar.Location = new System.Drawing.Point(87, 31);
+            this.txt_buscar.Name = "txt_buscar";
+            this.txt_buscar.Size = new System.Drawing.Size(416, 20);
+            this.txt_buscar.TabIndex = 8;
+            this.txt_buscar.TextChanged += new System.EventHandler(this.txt_buscar_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nombre";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dtg_Tienda);
+            this.groupBox3.Location = new System.Drawing.Point(12, 259);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(528, 206);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Listar";
+            // 
+            // dtg_Tienda
+            // 
+            this.dtg_Tienda.AllowUserToAddRows = false;
+            this.dtg_Tienda.AllowUserToDeleteRows = false;
+            this.dtg_Tienda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_Tienda.Location = new System.Drawing.Point(0, 20);
+            this.dtg_Tienda.Name = "dtg_Tienda";
+            this.dtg_Tienda.ReadOnly = true;
+            this.dtg_Tienda.Size = new System.Drawing.Size(528, 186);
+            this.dtg_Tienda.TabIndex = 0;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrar.Location = new System.Drawing.Point(445, 471);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(95, 29);
+            this.btnCerrar.TabIndex = 21;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click_1);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(315, 473);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(95, 29);
+            this.btnEliminar.TabIndex = 20;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(214, 473);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(95, 27);
+            this.btnCancelar.TabIndex = 19;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click_1);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.Location = new System.Drawing.Point(113, 473);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(95, 27);
+            this.btnGuardar.TabIndex = 18;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevo.Location = new System.Drawing.Point(12, 473);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(95, 27);
+            this.btnNuevo.TabIndex = 17;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Frm_Tienda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 562);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Frm_Tienda";
+            this.Text = "Principal";
+            this.Load += new System.EventHandler(this.Principal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Tienda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_descripcion;
+        private System.Windows.Forms.TextBox txt_precio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_buscar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dtg_Tienda;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
---
+++ Presentacion.Tienda/Principal.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logica.Tienda;
+using AccesoDatos;
+using Entidades.Tienda;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Presentacion.Tienda
+{
+    public partial class Frm_Tienda : Form
+    {
+        private ProductosLogica _productologica;
+        private string banderaGuardar = "";
+        private int id = 0;
+        public Frm_Tienda()
+        {
+            InitializeComponent();
+            _productologica = new ProductosLogica();
+        }
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            ControlarBotones(true, false, false, true, true);
+            ControlarCuadros(true);
+            LlenarProductos("");
+        }
+        private void LlenarProductos(string valor)
+        {
+            dtg_Tienda.DataSource = _productologica.ObtenerProductos(valor);
+        }
+        private void GuardarProductos()
+        {
+            Productos nuevoProducto = new Productos();
+            nuevoProducto.IDProducto = 0;
+            nuevoProducto.Nombre = txt_nombre.Text;
+            nuevoProducto.Descripcion = txt_descripcion.Text;
+            nuevoProducto.Precio = decimal.Parse(txt_precio.Text);
+
+            var validar = _productologica.ValidarProducto(nuevoProducto);
+            if (validar.Item1)
+            {
+                _productologica.GuardarProducto(nuevoProducto);
+                LlenarProductos("");
+                LimpiarCuadros();
+                ControlarBotones(true, false, false, true, true);
+                ControlarCuadros(true);
+            }
+            else
+            {
+                MessageBox.Show(validar.Item2, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ControlarBotones(Boolean nuevo, Boolean guardar, Boolean cancelar, Boolean eliminar, Boolean cerrar)
+        {
+            btnNuevo.Enabled = nuevo;
+            btnGuardar.Enabled = guardar;
+            btnCancelar.Enabled = cancelar;
+            btnEliminar.Enabled = eliminar;
+            btnCerrar.Enabled = cerrar;
+        }
+        private void ControlarCuadros(Boolean estado)
+        {
+            txt_nombre.Enabled = estado;
+            txt_descripcion.Enabled = estado;
+            txt_precio.Enabled = estado;
+            txt_buscar.Enabled = estado;
+        }
+        private void LimpiarCuadros()
+        {
+            txt_nombre.Text = "";
+            txt_descripcion.Text = "";
+            txt_precio.Text = "";
+        }
+        private void ActualizarCategoria()
+        {
+
+            Productos nuevoProducto = new Productos();
+            nuevoProducto.IDProducto= id;
+            nuevoProducto.Nombre = txt_nombre.Text;
+            nuevoProducto.Descripcion = txt_descripcion.Text;
+            nuevoProducto.Precio = decimal.Parse(txt_precio.Text);
+
+            var validar = _productologica.ValidarProducto(nuevoProducto);
+            if (validar.Item1)
+            {
+                _productologica.ActualizarProducto(nuevoProducto);
+                LlenarProductos("");
+                LimpiarCuadros();
+                ControlarBotones(true, false, false, true, true);
+                ControlarCuadros(true);
+            }
+            else
+            {
+                MessageBox.Show(validar.Item2, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            LlenarProductos(txt_buscar.Text);
+        }
+        public void Eliminar()
+        {
+            if (MessageBox.Show("¿Desea eliminar la categoria seleccionada", "Eliminar categoria?",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                var id = dtg_Tienda.CurrentRow.Cells["id"].Value.ToString();
+                _productologica.EliminarProducto(int.Parse(id));
+            }
+        }
+
+        private void dtgCategoria_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ControlarBotones(false, true, true, false, false);
+            ControlarCuadros(true);
+            txt_nombre.Focus();
+
+            txt_nombre.Text = dtg_Tienda.CurrentRow.Cells["nombre"].Value.ToString();
+            txt_descripcion.Text = dtg_Tienda.CurrentRow.Cells["descripcion"].Value.ToString();
+            txt_precio.Text = dtg_Tienda.CurrentRow.Cells["precio"].Value.ToString();
+            id = int.Parse(dtg_Tienda.CurrentRow.Cells["idproducto"].Value.ToString());
+            banderaGuardar = "actualizar";
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            ControlarBotones(false, true, true, false, false);
+            ControlarCuadros(true);
+            txt_nombre.Focus();
+            banderaGuardar = "guardar";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (banderaGuardar == "guardar")
+            {
+                GuardarProductos();
+                LlenarProductos("");
+            }
+            else if (banderaGuardar == "actualizar")
+            {
+                ActualizarCategoria();
+                LlenarProductos("");
+            }
+        }
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            ControlarBotones(true, false, false, true, true);
+            ControlarCuadros(false);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+            LlenarProductos("");
+        }
+
+        private void btnCerrar_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txt_buscar_TextChanged(object sender, EventArgs e)
+        {
+            LlenarProductos(txt_buscar.Text);
+        }
+    }
+}
